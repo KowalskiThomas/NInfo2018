8d2ed12b3063d3639b15e2fb80fcd7818e538463
Chacky <th.alban@outlook.fr>
2018-12-07 00:46:21 +0100

    test d'alcoolémie 4 - l'heure du décès n'est annoncé que s'il y a eu un décès

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -35,10 +35,16 @@ export class HomePage {
         {
           text: 'Save',
           handler: data => {
-            if (this.catchPhrase === "") this.catchPhrase = data;
-            else if (this.catchPhrase !== data && this.tmp === 0) {
+            if (this.catchPhrase === "") {
+              this.catchPhrase = data.title;
+              console.log(this.catchPhrase);
+            }
+            else if (this.catchPhrase !== data.title && this.tmp === 0) {
               this.drunkTime = this.drunkTime.toLocaleString();
               this.tmp++;
+              document.getElementById('dead').removeAttribute('disabled');
+              console.log(this.drunkTime);
+              console.log(this.tmp);
             }
             console.log('Saved clicked');
           }
@@ -48,9 +54,25 @@ export class HomePage {
     prompt.present();
   }
 
+  showDrunk() {
+    const prompt = this.alertCtrl.create({
+      title: "Heure du décès",
+      message: "Le verre de trop a été pris vers : \n" + drunkTime,
+      buttons: [
+        {
+          text: 'OK',
+          handler: data => {
+            console.log('OK clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
   initialise() {
     this.drunkTime = "";
-    console.log(this.drunkTime);
+    console.log('Initialise clicked');
   }
 
 }
