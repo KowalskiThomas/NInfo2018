13bfd45f97151fe10c7c146883554aa8e1061594
remi huguet <remi.huguet@ensiie.fr>
2018-12-07 07:31:08 +0100

    fix rer and weather

--- app/src/pages/rer/rer.ts
+++ app/src/pages/rer/rer.ts
@@ -13,7 +13,7 @@ export class RerPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
     this.trains = null;
-    this.http.get('http://51.75.94.199:5000/rer/next/').map(res => res.json()).subscribe(data => {
+    this.http.get('http://51.75.94.199:5000/rer/next').map(res => res.json()).subscribe(data => {
       this.trains = data.response.response;
       console.log(this.trains);
     });
